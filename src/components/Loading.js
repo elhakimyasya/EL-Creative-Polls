c2664df264175a1976a83d90b3df39f2bba232ea
Yasya El Hakim <yasyaelhakim@gmail.com>
2020-06-23 03:49:22 +0700

    Update

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CircularProgress from 'material-ui/CircularProgress';
-
-export default (props) => (
-    <div className="text-xs-center" style={props.loading ? {} : { display: 'none' }}>
-        <br />
-        <CircularProgress />
-    </div>
-);
