7c7be69afb9b4df3fe5c387faaa41be31f3d56e0
Yasya El Hakim <yasyaelhakim@gmail.com>
2020-06-23 00:27:50 +0700

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,11 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBCl_Bof8W2baR1-6mC5QOKQe06UfPwTNk",
-    authDomain: "polls-66eef.firebaseapp.com",
-    databaseURL: "https://polls-66eef.firebaseio.com",
-    storageBucket: "polls-66eef.appspot.com",
+    apiKey: "AIzaSyCYAMjf1YlWxQ0lBzHikORvstH_TBbiLBw",
+    authDomain: "polling-d7d0f.firebaseapp.com",
+    databaseURL: "https://polling-d7d0f.firebaseio.com",
+  
+    storageBucket: "polling-d7d0f.appspot.com",
 };
 
 export const firebaseApp = firebase.initializeApp(config);
